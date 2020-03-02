6ce14dac53502e0f5bae46575471d5b3b6ef95c3
Angie Avilan <angie.a.tellez@gmail.com>
2020-03-02 13:29:46 -0500

    buscador

--- js/buscador_interno.js
+++ js/buscador_interno.js
@@ -1,14 +1,14 @@
 
   function search() {
-      var input, filter, ul, li, i, h5, p;
+      var input, filter, ul, li, i, h3, p;
       input = document.getElementById("input");
       filter = input.value.toUpperCase();
       ul = document.getElementById("ul");
       li = ul.getElementsByTagName("li");
       for (i = 0; i < li.length; i++) {
-          h5 = li[i].getElementsByTagName("h5")[0];
-          console.log(h5 + "--" + filter)
-          if (h5.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          h5 = li[i].getElementsByTagName("h3")[0];
+          console.log(h3 + "--" + filter)
+          if (h3.innerHTML.toUpperCase().indexOf(filter) > -1) {
               li[i].style.display = "block";
           } else {
               li[i].style.display = "none";
