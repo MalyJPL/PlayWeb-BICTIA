af3d735cbdb8d3ca774de5c5e0a8e01a9f993a27
MalyJPL <malyjpl@gmail.com>
2020-03-02 22:44:20 -0500

    ajustes en cartas para usar un solo codigo de llenado html

--- js/buscador_interno.js
+++ js/buscador_interno.js
@@ -1,14 +1,14 @@
 
   function search() {
-      var input, filter, ul, li, i, h3, p;
+      var input, filter, ul, li, i, h5, p;
       input = document.getElementById("input");
       filter = input.value.toUpperCase();
       ul = document.getElementById("ul");
       li = ul.getElementsByTagName("li");
+      console.log(li[0])
       for (i = 0; i < li.length; i++) {
-          h5 = li[i].getElementsByTagName("h3")[0];
-          console.log(h3 + "--" + filter)
-          if (h3.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          h5 = li[i].getElementsByTagName("h5")[0]     
+          if (h5.innerHTML.toUpperCase().indexOf(filter) > -1) {
               li[i].style.display = "block";
           } else {
               li[i].style.display = "none";
